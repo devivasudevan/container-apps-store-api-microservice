e0c3219fd2647ce57b27c22d72d24cac0bfd1edb
devivasudevan <49675305+devivasudevan@users.noreply.github.com>
2022-04-25 13:45:40 +0200

    Update index.js

--- node-service/routes/index.js
+++ node-service/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Container App Demo' });
+  res.render('index', { title: 'Container App Demo v2' });
 });
 
 module.exports = router;
